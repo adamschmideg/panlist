83eea5a3133468cc97c1db193eb1cbe9e6d5e7bc
Adam Schmideg <adam@schmideg.net>
2016-11-09 22:20:15 +0100

    keep order of cards

--- src/panlist/trello.clj
+++ src/panlist/trello.clj
@@ -3,16 +3,28 @@
     [cheshire.core :as json]
     [clojure.set :as set]))
 
-;; TODO: keep order of cards and lists
+(defn add-index
+  "Add an index column called key, :index by default"
+  ([coll key]
+   (map-indexed (fn [idx elem] (assoc elem key idx))
+               coll))
+  ([coll] (add-index coll :index)))
+
+;; TODO: convert to a nested list of content only
 (defn load-board
   [board-file]
   (let [board (-> board-file slurp (json/parse-string true))
-        lists (map #(select-keys % [:name :id]) (:lists board))
-        cards (map #(select-keys % [:name :desc :idList]) (:cards board))
-        joined (set/join (set/rename lists {:name :listName})
-                         cards
-                         {:id :idList})
-        narrowed (map #(select-keys % [:name :desc :listName]) joined)]
-    (group-by :listName narrowed)))
+        lists (as-> board $
+                    (:lists $)
+                    (add-index $ :list-index)
+                    (set/project $ [:name :id :list-index])
+                    (set/rename $ {:name :listName}))
+        cards (as-> board $
+                    (:cards $)
+                    (add-index $ :card-index)
+                    (set/project $ [:name :desc :idList :card-index]))
+        joined (set/join lists cards {:id :idList})
+        sorted (sort-by (juxt :list-index :card-index) joined)]
+    (group-by :listName sorted)))
 
 
