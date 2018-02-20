c794f58990ac9cbfcf2103c18b0579ba16ef693b
Adam Schmideg <adam@schmideg.net>
2018-02-20 14:15:23 +0100

    group-by-week

--- src/panlist/calendar.clj
+++ src/panlist/calendar.clj
@@ -6,6 +6,12 @@
         _ (.add d2 Calendar/DAY_OF_YEAR 1)]
     d2))
 
+(defn zero-based-day-of-week [day]
+  (dec (.get day Calendar/DAY_OF_WEEK)))
+
+(defn get-month [day]
+  (.get day Calendar/MONTH))
+
 (defn days [start ndays]
   (loop [the-days [start]
          current (next-day start)
@@ -16,5 +22,20 @@
         (conj the-days current)
         (next-day current)
         (dec ndays)))))
+
+(defn group-by-week [dates]
+  (let [from-monday (drop
+                      (zero-based-day-of-week (first dates))
+                      dates)]
+    (partition 7 from-monday)))
+
+(defn group-by-month [weeks]
+  (partition-by #(get-in % [0 0])
+    weeks))
+
+(defn yearly-calendar [year]
+  (let [start (Calendar/getInstance)
+        _ (.set start year Calendar/JANUARY 1)]
+    (-> start (days 365) group-by-week group-by-month)))
 
 
